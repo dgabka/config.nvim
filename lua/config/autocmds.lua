7615778d9211843c2edaef488efe7fb365059980
Dawid Gąbka <gabka.daw@gmail.com>
2026-01-25 04:09:12 +0100

    implement core notes review workflow
    added :Notes command with following subcommands
      - pull - stashes current changes and pulls from remote
      - push - add all changes, commits with date and pushes to remote
      - review - starts an inbox review process, by adding all inbox notes
    to qflist and setting useful keymaps:
        - <leader>nm - move note with interactive picker
        - <leader>nd - deletes notes
        - <leader>fr - stops review process (closes qflist, removes keymaps)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -32,20 +32,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 2 -- insert 2 spaces for a tab
   end,
 })
-
-vim.api.nvim_create_user_command("NotesGitPush", function()
-  local notes_dir = os.getenv "OBSIDIAN_VAULT"
-  if not notes_dir or notes_dir == "" then
-    vim.notify("OBSIDIAN_VAULT is not set.", vim.log.levels.ERROR)
-    return
-  end
-  local date = os.date "%Y-%m-%d"
-  local commit_msg = date .. " notes"
-  -- Save current directory to restore later
-  local cwd = vim.fn.getcwd()
-  vim.cmd("cd " .. notes_dir)
-  vim.cmd "G add ."
-  vim.cmd("G commit -m '" .. commit_msg .. "'")
-  vim.cmd "G push"
-  vim.cmd("cd " .. cwd)
-end, { desc = "Stage, commit, and push notes using Fugitive" })
